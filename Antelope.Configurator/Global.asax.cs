aabc8c31db057a582c77d9156060c3045c82b8f5
quangnle <mnest2409@yahoo.com>
2015-11-24 19:48:51 +0800

    commit

--- Antelope.Configurator/Global.asax.cs
+++ Antelope.Configurator/Global.asax.cs
@@ -22,6 +22,8 @@ namespace Antelope.Configuator
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.DefaultNamespaces.Add("Antelope.Data.Controllers");
         }
     }
 }
