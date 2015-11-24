023665ef5f4a2ace68b8d6d829d72959166e3640
quangnle <mnest2409@yahoo.com>
2015-11-24 19:19:56 +0800

    construct the code

---
+++ Antelope.Remoter/Remoter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antelope.Remoter
+{
+    public class Remoter
+    {
+        public double GetBalance(int idAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MakeATransaction(int idFromAccount, int idToAccount, double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
