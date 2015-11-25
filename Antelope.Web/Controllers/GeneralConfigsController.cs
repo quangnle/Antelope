f48325032867852e4aa5968186d2b4492e41cdac
chungvodim <ba198647@gmail.com>
2015-11-25 18:27:34 +0800

    add more controllers

---
+++ Antelope.Web/Controllers/GeneralConfigsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Antelope.Data.Models;
+
+namespace Antelope.Web.Controllers
+{
+    public class GeneralConfigsController : Controller
+    {
+        private MainModel db = new MainModel();
+
+        // GET: GeneralConfigs
+        public ActionResult Index()
+        {
+            return View(db.GeneralConfigs.ToList());
+        }
+
+        // GET: GeneralConfigs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GeneralConfig generalConfig = db.GeneralConfigs.Find(id);
+            if (generalConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(generalConfig);
+        }
+
+        // GET: GeneralConfigs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GeneralConfigs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DefaultMonitorPeriod,DefaultRetryTimes")] GeneralConfig generalConfig)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GeneralConfigs.Add(generalConfig);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(generalConfig);
+        }
+
+        // GET: GeneralConfigs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GeneralConfig generalConfig = db.GeneralConfigs.Find(id);
+            if (generalConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(generalConfig);
+        }
+
+        // POST: GeneralConfigs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DefaultMonitorPeriod,DefaultRetryTimes")] GeneralConfig generalConfig)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(generalConfig).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(generalConfig);
+        }
+
+        // GET: GeneralConfigs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GeneralConfig generalConfig = db.GeneralConfigs.Find(id);
+            if (generalConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(generalConfig);
+        }
+
+        // POST: GeneralConfigs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GeneralConfig generalConfig = db.GeneralConfigs.Find(id);
+            db.GeneralConfigs.Remove(generalConfig);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Antelope.Web/Controllers/TargetConfigsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Antelope.Data.Models;
+
+namespace Antelope.Web.Controllers
+{
+    public class TargetConfigsController : Controller
+    {
+        private MainModel db = new MainModel();
+
+        // GET: TargetConfigs
+        public ActionResult Index()
+        {
+            return View(db.TargetConfigs.ToList());
+        }
+
+        // GET: TargetConfigs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TargetConfig targetConfig = db.TargetConfigs.Find(id);
+            if (targetConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(targetConfig);
+        }
+
+        // GET: TargetConfigs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TargetConfigs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IdAccount,IdTarget,MaxLimit")] TargetConfig targetConfig)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TargetConfigs.Add(targetConfig);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(targetConfig);
+        }
+
+        // GET: TargetConfigs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TargetConfig targetConfig = db.TargetConfigs.Find(id);
+            if (targetConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(targetConfig);
+        }
+
+        // POST: TargetConfigs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IdAccount,IdTarget,MaxLimit")] TargetConfig targetConfig)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(targetConfig).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(targetConfig);
+        }
+
+        // GET: TargetConfigs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TargetConfig targetConfig = db.TargetConfigs.Find(id);
+            if (targetConfig == null)
+            {
+                return HttpNotFound();
+            }
+            return View(targetConfig);
+        }
+
+        // POST: TargetConfigs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TargetConfig targetConfig = db.TargetConfigs.Find(id);
+            db.TargetConfigs.Remove(targetConfig);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
