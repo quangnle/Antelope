5ea807c3dfa19b62051bbe8657272b9eca4c8d20
Kong Nguyen <shenlongbk@gmail.com>
2015-11-27 17:23:22 +0800

    remove wpfclient project

---
+++ Antelope.Data/XModels/XMainModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antelope.Data.Models
+{
+    public partial class MainModel
+    {
+        public void ReloadAllDatabase()
+        {
+            var context = ((IObjectContextAdapter)this).ObjectContext;
+            var refreshableObjects = this.ChangeTracker.Entries().Select(c => c.Entity).ToList();
+            context.Refresh(RefreshMode.StoreWins, refreshableObjects);
+        }
+    }
+}
